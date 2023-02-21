9fd481dcd08c2c8db8d0e3900f4f73b8c9e6ab24
Minnathullah-TheMysterious <minnathullahmohammed@gmail.com>
2023-02-21 14:56:49 +0530

    added dark mode and light mode features

--- calc.js
+++ calc.js
@@ -7,18 +7,22 @@
 // };
 // a.addEventListener("click", e);
 
+function backspace() {
+  let exp = document.getElementById("display").innerText;
+  let result = exp.substring(0, exp.length - 1);
+  document.getElementById("display").innerText = result;
+}
 
-function backspace(){  
-    let exp = document.getElementById('display').innerText;  
-    let result = exp.substring(0, exp.length - 1)
-    document.getElementById('display').innerText = result
-} 
+// Use equal() function to return the result based on passed values.
+function equalTo() {
+  let solve = document.getElementById("display").innerText;
+  if (solve) {
+    document.getElementById("display").innerText = eval(solve);
+  }
+}
 
-  
-// Use equal() function to return the result based on passed values.  
-function equalTo(){
-    let solve = document.getElementById('display').innerText; 
-        if(solve){  
-            document.getElementById('display').innerText = eval(solve)  
-        }  
-}  
+changeMode = () => {
+    let mybody = document.body
+    console.log(mybody)
+    mybody.classList.toggle('darkMode')
+};
