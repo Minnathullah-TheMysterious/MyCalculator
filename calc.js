d32b83429ac7beee14576e0d495719c0e648f7a7
Minnathullah-TheMysterious <minnathullahmohammed@gmail.com>
2023-02-21 14:11:31 +0530

    responsiveness is added

--- calc.js
+++ calc.js
@@ -1,18 +1,24 @@
-// Use insert() function to insert the number in display.  
-function insert(num){  
-    document.form1.display.value = document.form1.display.value + num;  
-}  
+/* ******* by using eventListeners  ****** */
+// let a = document.getElementById("backspace");
+// let e = function(){
+//   let text = document.getElementById("display").innerText;
+//   let result = text.substring(0, text.length - 1);
+//   document.getElementById("display").innerText = result;
+// };
+// a.addEventListener("click", e);
+
+
+function backspace(){  
+    let exp = document.getElementById('display').innerText;  
+    let result = exp.substring(0, exp.length - 1)
+    document.getElementById('display').innerText = result
+} 
+
   
 // Use equal() function to return the result based on passed values.  
-function equal(){
-    var exp = document.form1.display.value;  
-        if(exp){  
-            document.form1.display.value = eval(exp)  
+function equalTo(){
+    let solve = document.getElementById('display').innerText; 
+        if(solve){  
+            document.getElementById('display').innerText = eval(solve)  
         }  
-}  
-  
-/* Here, we create a backspace() function to remove the number at the end of the numeric series in display. */  
-function backspace(){  
-    var exp = document.form1.display.value;  
-    document.form1.display.value = exp.substring(0, exp.length - 1); /* remove the element from total length ? 1 */  
 }  
